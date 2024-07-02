120b3f620436ad38cd397bd4109f2c35ad2da72f
Nova <bluewallscrashing@gmail.com>
2024-07-01 17:18:14 -0700

    Added Grounded Tracking
    - Added Grounded Tracking
    Now, he plays a "Jump" animation when he's switching lanes.
    Need to actually get him to jump now, and not switch lanes so instantly.

--- SBIG24/Assets/Scripts/PlayerMovement.cs
+++ SBIG24/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public GameManagerScript gameManagerScript;
+    public Animator animator;
+    private bool isGrounded;
+
     //Which wire the player is on, initialized at 0(Middle).
         //-1 = Left
         //1 = Right
     private int PlayerPos = 0;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+        //Debug.Log("IsGrounded: " + isGrounded);
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isGrounded){
+            Jump();
             GoLeft();
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+        else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))&& isGrounded){
+            Jump();
             GoRight();
         }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.name == "Wire(Clone)")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void Jump()
+    {
+        animator.SetTrigger("Jump");
     }
 
     void GoLeft()
